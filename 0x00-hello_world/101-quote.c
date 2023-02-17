6068de029a2a9e0b01fc9ad65f0cf160f89393be
Ayo Idoko <ayo.idoko007@gmail.com>
2023-02-16 17:27:37 -0800

    Updated

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char str[] = "Dora Korpar, 2015-10-19";
+fprintf(stderr, "and that piece of art is useful\" - %s\n", str);
+return (1);
+}
