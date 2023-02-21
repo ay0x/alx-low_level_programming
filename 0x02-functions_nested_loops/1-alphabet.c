cb16973fb177676ae7c75d1612df002c1320356e
Ayo Idoko <ayo.idoko007@gmail.com>
2023-02-21 12:52:55 -0800

    Updated

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+for (int i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+}
