b2b70d3ef672b3aa841e6aad8892c3f63a6e3d8e
Ayo Idoko <ayo.idoko007@gmail.com>
2023-02-21 14:14:09 -0800

    Updated

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,10 +5,10 @@
  * Return: Always 0.
  */
 void print_alphabet(void)
-{
-for (int i = 'a'; i <= 'z'; i++)
-{
-_putchar(i);
-}
-_putchar('\n');
-}
+	{
+	for (char i = 'a'; i <= 'z'; i++)
+		{
+		_putchar(i);
+		}
+	_putchar('\n');
+	}
