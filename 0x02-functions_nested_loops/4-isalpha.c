1a89a9d5c6a2a41327f46bcb5a07031fb824d2bc
Ayo Idoko <ayo.idoko007@gmail.com>
2023-02-22 05:40:02 -0800

    Updated

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * Description: checks character is a letter, lowercase or uppercase
+ * @c: the integer value it receives
+ * Return: 1 if true. 0 if false.
+ */
+
+int _isalpha(int c)
+{
+if ((c >= 97) | (c <= 122) && (c >= 65) | (c <= 90))
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
+
