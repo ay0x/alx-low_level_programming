87c4511cf695ed3c774c19e3afb8d307ff2c72f7
Ayo Idoko <ayo.idoko007@gmail.com>
2023-02-23 18:19:32 -0800

    Updated

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,6 +1,19 @@
-#include "main"
+#include "main.h"
 
 /**
  * print_line - Entry point
  * Description: Draws a straight line in the terminal
+ * @n: Number of line
+ * Return: void
  */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
