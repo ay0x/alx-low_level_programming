0113d4851744eec936aadcbc1b3c2a6b1aa6e699
Ayo Idoko <ayo.idoko007@gmail.com>
2023-02-27 09:44:16 -0800

    Updated

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - Entry point
+ * Description: Changes variable 'n' value to '98'
+ * @n: The memory address of 'n'
+ * Return: No return value
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
