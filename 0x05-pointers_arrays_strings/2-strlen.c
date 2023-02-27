97195aa2cef3a76f6b48d6a79ca9736c377ae314
Ayo Idoko <ayo.idoko007@gmail.com>
2023-02-27 14:05:55 -0800

    Updated

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - Entry point
+ * Description: Displays the length of a string
+ * @s: Characters from string
+ * Return: The length of the string characters
+*/
+
+int _strlen(char *s)
+{
+	int len = strlen(s);
+
+	return (len);
+}
