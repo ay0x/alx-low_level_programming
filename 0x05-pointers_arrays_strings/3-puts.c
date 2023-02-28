f4cee5f94f55b6839105805c175994bf4246b783
Ayo Idoko <ayo.idoko007@gmail.com>
2023-02-27 16:21:12 -0800

    Updated

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,20 +1,17 @@
 #include "main.h"
 
 /**
- * _puts - Entry point
- * Description: Function that prints a string
- * @str: String to pass
- * Return: void
+ * _puts - prints strings
+ * @str: the string
  */
-
 void _puts(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '0\')
+	while (str[i] != '\0')
 	{
-	_putchar(str[i]);
-	i++;
+		_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
