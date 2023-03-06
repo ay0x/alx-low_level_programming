1295b896812d7b7d0f2c0436175e418de16a2252
Ayo Idoko <ayo.idoko007@gmail.com>
2023-03-06 09:34:18 -0800

    Updated

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy - Entry point
+ * Description: copies memory area.
+ * @dest: Destination Pointer
+ * @src: Source pointer
+ * @n: Unsigned integer
+ * Return: Pointer
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	if (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+
+	return (dest);
+}
