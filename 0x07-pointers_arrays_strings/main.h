58d099b4cc8d7fb4537682134394657c49058ce0
Ayo Idoko <ayo.idoko007@gmail.com>
2023-03-07 12:15:01 -0800

    Updated

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,10 +4,6 @@
 #include <stdio.h>
 #include <string.h>
 
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
-
+void set_string(char **s, char *to);
 #endif /* MAIN_H */
