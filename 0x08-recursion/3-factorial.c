3b48c819d66e4df339b2e82b8d0c2b8a8dc963ef
Ayo Idoko <ayo.idoko007@gmail.com>
2023-03-08 11:51:28 -0800

    Updated

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * factorial - Entry point
+ * Description: Returns the factorial of a given number
+ * @n: integer
+ * Return: integer
+ */
+int factorial(int n)
+{
+	int i;
+
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		i = n * factorial(n - 1);
+	}
+			return (i);
+
+}
