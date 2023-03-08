6fe5f4cd86609ce3831a1c8b7a1ebc6062840f66
Ayo Idoko <ayo.idoko007@gmail.com>
2023-03-08 06:17:45 -0800

    Updated

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,7 +8,7 @@ int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-void _puts(char *str);
 char *_strcpy(char *dest, char *src);
+void _puts(char *str);
 
 #endif /* MAIN_H */
