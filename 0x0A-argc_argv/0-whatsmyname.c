d854c7ce3056096839f106aff3291176b9d6710a
Ayo Idoko <ayo.idoko007@gmail.com>
2023-03-10 12:47:47 -0800

    Updated

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: Main program
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return(0);
+}
