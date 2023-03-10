0003dd37b5f7cca1b6d4d97dff22b447bbddb2e3
Ayo Idoko <ayo.idoko007@gmail.com>
2023-03-10 13:30:43 -0800

    Updated

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,9 +8,8 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
 	printf("%d\n", argc);
-	void(*argv)
-	return(0);
+	return (0);
 }
