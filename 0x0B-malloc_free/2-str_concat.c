a3adb3f53a8608a89d80edfa555807a002b802b3
Ayo Idoko <ayo.idoko007@gmail.com>
2023-03-14 14:54:06 -0700

    Updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,41 +10,35 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-char *concat;
-int str_len1 = 0;
-int str_len2 = 0;
-int i = 0;
-int j = 0;
-int total_len;
+	int i, j, len1, len2, len;
+	char *result;
 
-if (s1 == NULL)
-	s1 = "";
-if (s2 == NULL)
-	s2 = "";
+	len1 = len2 = 0;
 
-while (s1[str_len1] != '\0')
-{
-	str_len1++;
-}
-while (s2[str_len2] != '\0')
-{
-	str_len2++;
-}
-total_len = str_len1 + str_len2;
+	if (s1 != NULL)
+	{
+		i = 0;
+		while (s1[i++] != '\0')
+			len1++;
+	}
 
-concat = (char *)malloc(sizeof(char) * total_len + 1);
+	if (s2 != NULL)
+	{
+		i = 0;
+		while (s2[i++] != '\0')
+			len2++;
+	}
 
-for (i = 0; i < str_len1; i++)
-{
-concat[i] = s1[i];
-}
+	len = len1 + len2;
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (result == NULL)
+		return (NULL);
 
-for (j = 0; j < str_len2; i++, j++)
-{
-	concat[i] = s2[j];
-}
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+	for (j = 0; j < len2; j++, i++)
+		result[i] = s2[j];
+	result[len] = '\0';
 
-if (concat == NULL)
-	return (NULL);
-return (concat);
+	return (result);
 }
