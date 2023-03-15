35b93bbad8cec0ac7d911bd38d6e4811709d8d15
Ayo Idoko <ayo.idoko007@gmail.com>
2023-03-15 13:20:16 -0700

    Updated

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * free_grid - Entry point
+ * Description: Function that frees a 2 dimensional grid.
+ * @grid: Number of grid
+ * @height: Number of height of the grid
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+int i;
+
+for (i = 0; i < height; i++)
+{
+	free(grid[i]);
+}
+free(grid);
+}
