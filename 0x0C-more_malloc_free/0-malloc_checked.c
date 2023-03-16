e13c48ee5b6d991077c130c54adb882efa740593
Ayo Idoko <ayo.idoko007@gmail.com>
2023-03-16 11:14:11 -0700

    Updated

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Entry point
+ * Description: Allocates memory
+ * @b: Number of memory
+ * Return: Pointer to the address
+ */
+
+void *malloc_checked(unsigned int b)
+{
+int *mallock = malloc(b);
+
+if (mallock == NULL)
+{
+exit (98);
+}
+return (mallock);
+}
