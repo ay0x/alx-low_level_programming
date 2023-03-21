4bee5664999dd3410740d5bfa22ffce7738032ef
Ayo Idoko <ayo.idoko007@gmail.com>
2023-03-20 22:48:40 -0700

    Update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - Dog struct
+ * @name: Name variable
+ * @age: Age variable
+ * @owner: Owner variable
+ *
+ * Description: Dog struct and its element
+*/
+
 struct dog
 {
 	char *name;
