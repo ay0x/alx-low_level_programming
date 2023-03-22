a52a25c77039e5fa864aa170f7452e597f31b689
Ayo Idoko <ayo.idoko007@gmail.com>
2023-03-22 11:14:49 -0700

    Updated

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,38 +3,53 @@
 #include <string.h>
 
 /**
- * new_dog- Entry point
- * Description: Function that creates a new dog
+ * new_dog - Function that creates a new dog
  * @name: Name string
  * @age: Age
  * @owner: Owner
+ *
  * Return: A brand new Dog
-*/
+ */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-char *name_copy;
-char *owner_copy;
-dog_t *dog;
+	unsigned int name_len, owner_len, i;
+	dog_t *new_dog;
 
-dog = malloc(sizeof(dog_t));
-if (dog == NULL)
-	return (NULL);
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	for (name_len = 0; name[name_len]; name_len++)
+		;
+	name_len++;
+	new_dog->name = malloc(name_len * sizeof(char));
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
 
-name_copy = strdup(name);
-owner_copy = strdup(owner);
+	for (i = 0; i < name_len; i++)
+		new_dog->name[i] = name[i];
 
-if (name_copy == NULL || owner_copy == NULL)
-{
-	free(dog);
-	free(name_copy);
-	free(owner_copy);
-	return (NULL);
-}
+	new_dog->age = age;
 
-dog->name = name_copy;
-dog->age = age;
-dog->owner = owner_copy;
+	for (owner_len = 0; owner[owner_len]; owner_len++)
+		;
+	owner_len++;
 
-return (dog);
+	new_dog->owner = malloc(owner_len * sizeof(char));
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+
+	for (i = 0; i < owner_len; i++)
+		new_dog->owner[i] = owner[i];
+
+	return (new_dog);
 }
