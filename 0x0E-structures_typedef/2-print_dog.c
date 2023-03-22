b776e4605d506ca34b281f45670e1604163f1c7e
Ayo Idoko <ayo.idoko007@gmail.com>
2023-03-21 22:55:22 -0700

    Upated

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,34 +11,17 @@
 
 void print_dog(struct dog *d)
 {
-
 	if (d != NULL)
 	{
-		if (d->name == NULL)
-		{
-			printf("Name: (nil)\n");
-		}
-		else
-		{
+		if (d->name != NULL)
 			printf("Name: %s\n", d->name);
-		}
-
-		if (d->age == 0.0)
-		{
-			printf("Age: (nil)\n";
-		}
 		else
-		{
-			printf("Age: %.1f\n", d->age);
-		}
-
-		if (d->owner == NULL)
-		{
-			printf("Owner: (nil)\n");
-		}
-		else
-		{
+			printf("Name: (nil)\n");
+		printf("Age: %.6f\n", d->age);
+		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
-		}
+		else
+			printf("Owner: (nil)\n");
 	}
 }
+
