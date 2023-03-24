ca1b67aae5537cda231389c054a93f8d01d276c4
Ayo Idoko <ayo.idoko007@gmail.com>
2023-03-23 19:51:14 -0700

    Updated

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,17 +18,12 @@ va_list my_list;
 
 va_start(my_list, n);
 
-if (separator == NULL)
-{
-	separator = "";
-}
-
 for (i = 0; i < n; i++)
 {
 	int num = va_arg(my_list, int);
 
 	printf("%d", num);
-	if (i != n - 1)
+	if (i != n - 1 && separator != NULL)
 	{
 	printf("%s", separator);
 	}
