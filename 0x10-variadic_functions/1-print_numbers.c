de06202eacbeea3c45ebb45ee527bad690d86d91
Ayo Idoko <ayo.idoko007@gmail.com>
2023-03-23 20:59:26 -0700

    Updated

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,7 +4,6 @@
 
 /**
  * print_numbers - Entry point
- * Description: Prints numbers, followed by a new line.
  * @separator: The string to be printed between numbers.
  * @n: The number of integers passed to the function.
  * @...: A variable number of numbers to be printed.
