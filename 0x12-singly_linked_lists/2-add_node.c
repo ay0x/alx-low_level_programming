d82b1eb1a447d5f2348759c28dd0629457f29b69
Ayo Idoko <ayo.idoko007@gmail.com>
2023-03-30 22:32:18 -0700

    Updated

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,15 +1,30 @@
-#include "lists.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include "lists.h"
 
 /**
- * add_node - Entry point
- * Description: Adds a new node at the beginning of a list_t list.
- * @head: Head pointer
- * @str: Data in the node
- * Return: Address of the new element, or NULL if it failed
-*/
+ * _strlen - function that returns the length of a string.
+ * @s : s is a character
+ * Return: value is i
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * add_node - add a new node at beginning of a list_t list.
+ * @head: head of a list_t list.
+ * @str: value to insert into element.
+ * Return: the number of nodes.
+ */
 
 list_t *add_node(list_t **head, const char *str)
 {
