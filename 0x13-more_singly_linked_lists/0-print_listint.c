53b4026d6c81d72185ce91f66735490161e28721
Ayo Idoko <ayo.idoko007@gmail.com>
2023-04-03 19:48:54 -0700

    Updated

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_listint - Entry point
+ * Description: Prints all the elements of a node.
+ * @h: Head pointer
+ * Return: Size of elements(nodes)
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	listint_t *ptr = (listint_t *)h;
+	size_t count = 0;
+
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		count++;
+		ptr = ptr->next;
+	}
+
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This is a README file for this repository to be updated later in the project.
