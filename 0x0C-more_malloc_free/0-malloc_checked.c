fd45743739dc488b2a618b8eefa8efb2bf03a27d
Ayo Idoko <ayo.idoko007@gmail.com>
2023-06-15 15:52:20 -0700

    Updated

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * malloc_checked - Entry point
+ * Description: Creates memory allocation. Returns 98 on malloc failure.
+ * @b: Malloc size
+ * return: pointer to memory alocation
+*/
+
 void *malloc_checked(unsigned int b)
 {
 	void *ptr = malloc(b);
