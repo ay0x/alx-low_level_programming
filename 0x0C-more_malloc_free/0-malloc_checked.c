56ce71391dd60f6fc7956bf9164502a1cdc7be6b
Ayo Idoko <ayo.idoko007@gmail.com>
2023-06-15 16:31:06 -0700

    Updated

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,7 +6,7 @@
  * Description: Creates memory allocation. Returns 98 on malloc failure.
  * @b: Malloc size
  * return: pointer to memory alocation
-*/
+ */
 
 void *malloc_checked(unsigned int b)
 {
