403a1d150739e3fe518bdc27cb78e74993dbcb6b
Ayo Idoko <ayo.idoko007@gmail.com>
2023-06-19 05:19:25 -0700

    Added comment

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: Prints file name using __FILE__
+ * predefine macros
+ * Return: 0
+*/
+
 int main(void)
 {
-printf("%s\n", __FILE__);
-return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
+
