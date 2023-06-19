985e6580812481135d14c0552fece2a101883bc9
Ayo Idoko <ayo.idoko007@gmail.com>
2023-06-19 11:13:47 -0700

    Updated

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is a README file to be uploaded later in the project.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - Struct for dog attributes
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Ownder of the dog
+ *
+ * Description: Struct to contain the attributes of a dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
