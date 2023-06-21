b82c43dc2c2bb1fde27df5680bf2486e40a0854b
Ayo Idoko <ayo.idoko007@gmail.com>
2023-06-21 08:37:35 -0700

    Updated

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 /**
  * print_name - Entry point
  * Description: Function that prints a name
@@ -8,7 +10,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (f)
+	if (f != NULL && name != NULL)
 	{
 		f(name);
 	}
