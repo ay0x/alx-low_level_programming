c4475c15d473698550baca41f78f8c16f9320684
Ayo Idoko <ayo.idoko007@gmail.com>
2023-07-04 15:10:15 -0700

    Updated

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees list.
+ * @head: pointer to the first element of the list .
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *next = NULL;
+
+	if (head == NULL)
+		return;
+		
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+}
---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a listint_t linked list
+ * @head: pointer to the first element of the lsit .
+ *
+ * Return: the head node's data (n).
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int n;
+
+	if (!*head)
+		return (0);
+	tmp = *head;
+	*head = tmp->next;
+	n = tmp->n;
+	free(tmp);
+	return (n);
+}
