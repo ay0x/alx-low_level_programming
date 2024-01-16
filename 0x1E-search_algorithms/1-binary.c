182d5ba944093c4bd25e16296fbc56c50e836431
Ayo Idoko <ayo.idoko007@gmail.com>
2024-01-16 23:43:33 +0200

    Added more conditions

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,29 +10,35 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left = 0;
-    size_t right = size - 1;
-    size_t mid, i;
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t mid, i;
 
-    while (left <= right)
-    {
-        mid = (left + right) / 2;
+	if (size  == 1)
+		return (array[left]);
 
-        printf("Searching in array: ");
-        for (i = left; i <= right; i++)
-        {
-            printf("%d", array[i]);
-            if (i < right)
-                printf(", ");
-        }
-        printf("\n");
-        
-        if (array[mid] == value)
-            return (mid);
-        else if (array[mid] < value)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
-    return (-1);
+	while (left <= right)
+	{
+		if (left == 0 && right == 0)
+			return (-1);
+
+		mid = (left + right) / 2;
+
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i < right)
+				printf(", ");
+		}
+		printf("\n");
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+	return (-1);
 }
