dd147786a3437468fda15c0da95f898c4341d211
Ayo Idoko <ayo.idoko007@gmail.com>
2024-01-16 23:48:07 +0200

    Updated

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,8 @@
 #ifndef __HEADER
 #define __HEADER
-#include <stdlib.h>
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
